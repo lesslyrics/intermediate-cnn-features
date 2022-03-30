28961ca63ea3a2ddcdffca49fd1511a3ed993a33
Lesslyrics <alina_boshenko@mail.ru>
2022-03-30 04:01:28 +0200

    updated exec file

--- exec.sh
+++ exec.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sudo apt install curl
+apt install curl
 
 curl https://testbucket-1h.s3.amazonaws.com/test.mp4 --output test.mp4
 
-python /feature_extraction.py --video_list videos.txt --network vgg --framework>
+python /feature_extraction.py --video_list videos.txt --network vgg --framework tensorflow --output_path test --tf_model vgg_16.ckpt
 
 rm test.mp4
 
